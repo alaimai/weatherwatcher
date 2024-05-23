31c06f094233778e3be3b533ab38e9bdcadb9a4a
Lei Zheng <zhenglei530@gmail.com>
2024-05-23 12:40:23 +1200

    delete christchurch in app

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,8 @@
-import Christchurch from './Christchurch'
-
 function App() {
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
-      <Christchurch />
     </div>
   )
 }
