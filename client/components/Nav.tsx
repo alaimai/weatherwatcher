c3d2a8ad900a2eebd1abd9b91bd1328494aeeb28
Christy Liu <liusiyang915@gmail.com>
2024-05-23 22:49:45 +1200

    resolving merging conflicts in Nav.tsx

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,12 +1,13 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
     <nav>
       <ul>
+        <li><Link to="/home">Home</Link></li>
         <li><Link to="/Auckland">Auckland</Link></li>
         <li><Link to="/Wellington">Wellington</Link></li>
-        <li><Link to="/Christcburch">Christchurch</Link></li>
+        <li><Link to="/Christchurch">Christchurch</Link></li>
         <li><Link to="/Invercargill">Invercargill</Link></li>
       </ul>
     </nav>
