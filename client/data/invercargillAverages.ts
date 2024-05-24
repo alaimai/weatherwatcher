a8e3dec0014d19f459878ab4ceacf2bbcc4f353d
Hope Robertson <hope.robertson.nz@gmail.com>
2024-05-24 13:16:21 +1200

    added monthly averages for invercargill weather

---
+++ client/data/invercargillAverages.ts
@@ -0,0 +1,21 @@
+interface MonthlyTemperature {
+  month: string
+  averageTemperature: number
+}
+
+const monthlyAverages: MonthlyTemperature[] = [
+  { month: 'jan', averageTemperature: 14.2 },
+  { month: 'feb', averageTemperature: 14.0 },
+  { month: 'mar', averageTemperature: 12.9 },
+  { month: 'apr', averageTemperature: 10.7 },
+  { month: 'may', averageTemperature: 8.6 },
+  { month: 'jun', averageTemperature: 6.3 },
+  { month: 'jul', averageTemperature: 5.7 },
+  { month: 'aug', averageTemperature: 6.6 },
+  { month: 'sep', averageTemperature: 8.4 },
+  { month: 'oct', averageTemperature: 9.7 },
+  { month: 'nov', averageTemperature: 11.1 },
+  { month: 'dec', averageTemperature: 13.2 },
+]
+
+export default monthlyAverages
