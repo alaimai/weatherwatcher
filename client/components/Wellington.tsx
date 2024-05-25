451aaa2d1549816c674b9e2eaf598eeead5f848a
Christy Liu <liusiyang915@gmail.com>
2024-05-26 01:50:01 +1200

    added some pics to Wellington.tsx

--- client/components/Wellington.tsx
+++ client/components/Wellington.tsx
@@ -1,12 +1,29 @@
 
 function Wellington() {
+  
   return (
-    <div className="Welly">
+    <div className="Welly-container">
       <h1>Wellington Today</h1>
       <h3>Temperature: 9°C - 13°C</h3>
       <h3>Partly Cloudy</h3>
       <p>Wellington is New Zealand&apos;s centre of government and the world&apos;s southernmost capital city. Wellington is also the country&apos;s cultural capital and the third most populous urban area in New Zealand. </p>
-    </div>
+   
+      <div className="WellyImage-container">
+      <img
+        src="https://wellingtonnz.bynder.com/transform/a2babfe9-9456-4806-a2d6-629f381f00fd/Cable-Car-02"
+        alt="Wellington2"
+      />
+      <img
+        src="https://wellingtonnz.bynder.com/transform/a2babfe9-9456-4806-a2d6-629f381f00fd/Cable-Car-02"
+        alt="Wellington2"
+      />
+      <img
+        src="https://www.newzealand.com/assets/Tourism-NZ/Wellington/5ea9f62ac6/img-1536234753-4281-1285-E39AD092-B2AD-CA2E-6C37ADEA48FB18C1__aWxvdmVrZWxseQo_CropResizeWzE5MDAsMTAwMCw3NSwianBnIl0.jpg"
+        alt="Wellington3"
+      />
+        
+      </div>
+    </div> 
   )
 }
 
