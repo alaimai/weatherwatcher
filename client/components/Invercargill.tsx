2c80bdc8d8b268665ad9cb708777381ad62423ed
Hope Robertson <hope.robertson.nz@gmail.com>
2024-05-26 00:34:54 +1200

    refactored to get text and images displaying differently

--- client/components/Invercargill.tsx
+++ client/components/Invercargill.tsx
@@ -13,6 +13,17 @@ function Invercargill() {
     ? invercargillAverages.find((month) => month.month === selectedMonth)
     : null
 
+  const johnText = `"According to my Uncle John, Friday 24 May has been pleasant with
+    some incoming cloud on the horizon. Considering the temperature in
+    Invercargill has hovered around 4-6°C today, this shows the positive
+    and hardy attitude of the people of Southland. There have been
+    multiple frosts already and it is now Rugby season."`
+  const maraeMessage = `"Murihiku Marae in Waihōpai/Invercargill. The name Murihiku was given to the Southland region, and means "the last joint of the tail". Early Māori of the Waitaha iwi were based on the coast. Later iwi to come to the region were Ngāti Māmoe and Kāi Tahu, from further north. Sealers and whalers arrived in the 1790s and from the 1850s immigrants from Britain."`
+  const towerMessage = `"Sunset behind the Invercargill Water Tower, built in the late 1880s. It was once open to the public, but closed in 2012 due to earthquake safety risks."`
+  const basilicaMessage = `"Basilica of St Mary in the snow in 2009. This Gothic Revival style
+    church was designed by celebrated architect Francis Petre, and
+    opened in 1905. It is of Catholic denomination and is part of the
+    Diocese of Dunedin."`
   return (
     <div>
       <h2>Invercargill/Waihōpai Weather</h2>
@@ -48,56 +59,43 @@ function Invercargill() {
         )}
         <div className="johnsWeatherReport">
           <h3>Live Weather from John M.</h3>
-          <p>
-            According to my Uncle John, Friday 24 May has been pleasant with
-            some incoming cloud on the horizon. Considering the temperature in
-            Invercargill has hovered around 4-6°C today, this shows the positive
-            and hardy attitude of the people of Southland. There have been
-            multiple frosts already and it is now Rugby season.
-          </p>
+          <p>{johnText}</p>
         </div>
-        <h3>Images</h3>
-        <div className="invercargillImageContainer">
-          <figure className="maraeImage">
+        <div className="images-section">
+          <h3>Images</h3>
+          <div className="image-text-pair">
             <img
               className="waihopai-marae"
               src="/images/waihopai-marae.jpg"
               alt="marae in waihōpai"
             />
-            <figcaption>
-              Murihiku Marae in Waihōpai/Invercargill. The name Murihiku was
-              given to the Southland region, and means the last joint of the
-              tail. Early Māori of the Waitaha iwi were based on the coast.
-              Later iwi to come to the region were Ngāti Māmoe and Kāi Tahu,
-              from further north. Sealers and whalers arrived in the 1790s and
-              from the 1850s immigrants from Britain.
-            </figcaption>
-          </figure>
-          <figure className="towerImage">
+            <div className="marae-image-text">
+              <h3>Waihopai Marae</h3>
+              <p>{maraeMessage}</p>
+            </div>
+          </div>
+          <div className="image-text-pair">
             <img
               className="inv-water-tower"
               src="/images/invercargill-1.jpg"
               alt="Sunset behind the Invercargill water tower"
             />
-            <figcaption>
-              Sunset behind the Invercargill Water Tower, built in the late
-              1880s. It was once open to the public, but closed in 2012 due to
-              earthquake safety risks.
-            </figcaption>
-          </figure>
-          <figure className="basilicaImage">
+            <div className="tower-image-text">
+              <h3>Invercargill Water Tower</h3>
+              <p>{towerMessage}</p>
+            </div>
+          </div>
+          <div className="image-text-pair">
             <img
               className="inv-basilica"
               src="/images/inv-basilica.jpg"
               alt="Snow surrounding St Mary's basilica in 2009"
             />
-            <figcaption>
-              Basilica of St Mary in the snow in 2009. This Gothic Revival style
-              church was designed by celebrated architect Francis Petre, and
-              opened in 1905. It is of Catholic denomination and is part of the
-              Diocese of Dunedin.
-            </figcaption>
-          </figure>
+            <div className="basilica-image-text">
+              <h3>Catholic Basilica in Snow</h3>
+              <p>{basilicaMessage}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
